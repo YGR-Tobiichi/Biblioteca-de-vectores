d5c5602b85f89bdacbe7c507ba8b77fa0878e2cb
Tobiichi <tobiichi.ma@gmail.com>
2026-03-03 01:51:53 -0600

    commit

---
+++ ElProductoPunto/ElProductoPunto/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ElProductoPunto
+{
+    using ProyectoBiblioteca;
+    using System.Runtime.CompilerServices;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector();
+            Vector vector2 = new Vector();
+
+            Console.WriteLine("Ingrese componente x del vector 1.");
+            vector1.ComponenteX = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese componente y del vector 1.");
+            vector1.ComponenteY = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese componente x del vector 2.");
+            vector2.ComponenteX = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese componente y del vector 2.");
+            vector2.ComponenteY = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El producto punto de los vectores es: " 
+                + Vector.CalcularProductoPunto(vector1, vector2));
+            try
+            {
+                Console.WriteLine("La proyección del vector 1 sobre el vector 2 es: "
+                + Vector.CalcularProyeccionEscalar(vector1, vector2));
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProyectoBiblioteca/ProyectoBiblioteca/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace ProyectoBiblioteca
+{
+    public class Vector
+    {
+        private double componenteX;
+        private double componenteY;
+
+        public double ComponenteX { get => componenteX; set => componenteX = value; }
+        public double ComponenteY { get => componenteY; set => componenteY = value; }
+
+        public double Magnitud()
+        {
+            return Math.Sqrt(Math.Pow(ComponenteX, 2) + Math.Pow(ComponenteY, 2));
+        }
+
+        public static double CalcularProductoPunto(Vector vector1, Vector vector2)
+        {
+            return (vector1.ComponenteX * vector2.ComponenteX) + (vector1.ComponenteY * vector2.ComponenteY);
+        }
+
+        public static double CalcularProyeccionEscalar(Vector vector1, Vector vector2)
+        {
+            double productoPunto = CalcularProductoPunto(vector1, vector2);
+            double magnitudVector2 = vector2.Magnitud();
+            if (magnitudVector2 == 0)
+            {
+                throw new Exception("El vector de referencia no puede ser el vector cero.");
+            }
+            return productoPunto / magnitudVector2;
+        }
+    }
+}
