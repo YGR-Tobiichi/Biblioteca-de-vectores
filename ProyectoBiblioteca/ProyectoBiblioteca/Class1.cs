7e299ba17fce29b181d2d5690402e14a83241211
tobiichi <tobiichi.ma@gmail.com>
2026-03-04 18:42:28 -0600

    Agregar metodo para angulo entre vectores

--- ProyectoBiblioteca/ProyectoBiblioteca/Class1.cs
+++ ProyectoBiblioteca/ProyectoBiblioteca/Class1.cs
@@ -28,5 +28,34 @@
             }
             return productoPunto / magnitudVector2;
         }
+        public static double CalcularAngulo(Vector vector1, Vector vector2)
+        {
+            double productoPunto = CalcularProductoPunto(vector1, vector2);
+            double magnitudVector1 = vector1.Magnitud();
+            double magnitudVector2 = vector2.Magnitud();
+
+            if (magnitudVector1 == 0 || magnitudVector2 == 0)
+            {
+                throw new Exception("No se puede calcular el ángulo con un vector cero.");
+            }
+            double cosenoAngulo = productoPunto / (magnitudVector1 * magnitudVector2);
+            return Math.Acos(cosenoAngulo) * (180.0 / Math.PI);
+        }
+        public static Vector SumarVectores(Vector vector1, Vector vector2)
+        {
+            return new Vector
+            {
+                ComponenteX = vector1.ComponenteX + vector2.ComponenteX,
+                ComponenteY = vector1.ComponenteY + vector2.ComponenteY
+            };
+        }
+        public static Vector RestarVectores(Vector vector1, Vector vector2)
+        {
+            return new Vector
+            {
+                ComponenteX = vector1.ComponenteX - vector2.ComponenteX,
+                ComponenteY = vector1.ComponenteY - vector2.ComponenteY
+            };
+        }
     }
 }
--- ProyectoBiblioteca/ProyectoBiblioteca/obj/Debug/net10.0/ProyectoBiblioteca.AssemblyInfo.cs
+++ ProyectoBiblioteca/ProyectoBiblioteca/obj/Debug/net10.0/ProyectoBiblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoBiblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c5602b85f89bdacbe7c507ba8b77fa0878e2cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoBiblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoBiblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
